f8d264e763f3421b6fd88dcc521d3c55641dd5c5
amin10 <manna@mit.edu>
2018-01-19 03:34:52 -0500

    magenta cleanup

--- music_generation/run.sh
+++ music_generation/run.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt > /dev/null
 mkdir -p outputs
 # python magenta/models/melody_rnn/melody_rnn_generate.py --config attention_rnn --bundle_file ./attention_rnn.mag --output_dir ./outputs
 
-python magenta/models/melody_rnn/server.py --config attention_rnn --bundle_file ./attention_rnn.mag --output_dir ./outputs
+python melody_rnn/server.py --config attention_rnn --bundle_file ./attention_rnn.mag --output_dir ./outputs
