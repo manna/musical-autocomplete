0f06cda6573515b9fc6f61103d3e6cdda3d0a370
paperspace <manna@mit.edu>
2018-01-21 23:24:52 -0500

    remove install command from run.sh

--- music_generation/run.sh
+++ music_generation/run.sh
@@ -1,6 +1,4 @@
-pip install -r requirements.txt > /dev/null
-
 mkdir -p outputs
 # python magenta/models/melody_rnn/melody_rnn_generate.py --config attention_rnn --bundle_file ./attention_rnn.mag --output_dir ./outputs
 
-python melody_rnn/server.py --config attention_rnn --bundle_file ./attention_rnn.mag --output_dir ./outputs
+python2 melody_rnn/server.py --config attention_rnn --bundle_file ./attention_rnn.mag --output_dir ./outputs
